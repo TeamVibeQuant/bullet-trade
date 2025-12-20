0a08bdb8b360e560276e74065951130db08eec28
brucelee <13911566027@163.com>
2025-12-21 01:10:47 +0800

    Update version to 0.5.4  （PR #6）

--- bullet_trade/__version__.py
+++ bullet_trade/__version__.py
@@ -1,6 +1,6 @@
 """版本信息"""
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 __author__ = 'BulletTrade Team'
 __email__ = 'groundlee@gmail.com'
 __url__ = 'https://github.com/BulletTrade/bullet-trade'
